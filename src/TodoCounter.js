40533d91df30b7b863d67c90e770403eb13dc34e
Adrian <adspadrian@gmail.com>
2025-01-06 15:17:51 -0300

    commit

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -3,8 +3,12 @@ import './TodoCounter.css';
 function TodoCounter({ total, completed }){
     return (
         <h1>
-            Has completado {completed} de tus {total} ToDos
-        </h1>
+        <span className="todo-counter-text">Has completado </span>
+        <span className="bold-text">{completed}</span>
+        <span className="todo-counter-text"> de tus </span>
+        <span className="bold-text">{total}</span>
+        <span className="todo-counter-text"> ToDos</span>
+    </h1>
     );
 }
 export {TodoCounter};
